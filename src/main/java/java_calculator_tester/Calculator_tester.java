9a8783b26de197e5ec1e81d3372eff0cf1478b41
AngeloDima <Angelodimauro91@gmail.com>
2023-02-09 15:00:29 +0100

    BONUS MIO:    input utente  (SENZA CONTROLLI)

--- src/main/java/java_calculator_tester/Calculator_tester.java
+++ src/main/java/java_calculator_tester/Calculator_tester.java
@@ -1,19 +1,48 @@
 package java_calculator_tester;
 
+import java.util.Scanner;
+
 public class Calculator_tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner (System.in);
+		
 		Calc calcolatore = new Calc();
 		
-		int num1 = 15;
-		int num2 = 5;
+	
+		
+		System.out.println("Quale operazione vuoi eseguire? [1]Addizione  [2]Sottrazione [3]Moltiplicazione [4]Divisione");
+		int Opera = s.nextInt();
+		
+		System.out.print("Ok, dimmi il primo numero (INTERO): ");
+		int num1 = s.nextInt();
+		System.out.print("ora dimmi il secondo numero (INTERO): ");
+		int num2 = s.nextInt();
 		
 		
-		System.out.println(calcolatore.divide(num1, num2));
+		
+		//PARTONO GLI IF
 		
 		
 		
+		if (Opera == 1) {
+			System.out.println("il tuo risultato è: " + calcolatore.add(num1, num2));
+		}
+		
+		else if (Opera == 2) {
+			System.out.println("il tuo risultato è: " + calcolatore.subtract(num1, num2));
+		}
+		
+		else if (Opera == 3) {
+			System.out.println("il tuo risultato è: " + calcolatore.multiply(num1, num2));
+		}
+		
+		else if (Opera == 4) {
+			System.out.println("il tuo risultato è: " + calcolatore.divide(num1, num2));
+		}
+		
+			
 	}
 
 }
